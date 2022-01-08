0a20830d2fd3f03e3bb59e00c02851750778bf82
nrot <nrot13@gmail.com>
2022-01-08 20:31:03 +0300

    WIP

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,21 @@ use solana_sdk::{
 
 use std::path::{Path};
 
+fn to_lamports(sol: u64)->u64{
+    sol * 1000000000
+}
+
 fn main() {
     println!("Start example");
 
-    let client = RpcClient::new("http://localhost:8899".into());
-    let kp = read_keypair_file(Path::new("wallets/wallet1.json")).expect("Can`t read kp file");
-    let pb: Pubkey = kp.pubkey();
-    let bl = client.get_balance(&pb);
-    println!("Wallet 1 balance: {:?}", bl);
+    let client = RpcClient::new("https://api.devnet.solana.com".into());
+    let wal1 = read_keypair_file(Path::new("wallets/wallet1.json")).expect("Can`t read kp wallet 1 file");
+    println!("Wallet 1 balance: {:?}", client.get_balance(&wal1.pubkey()));
+
+    let wal2 = read_keypair_file(Path::new("wallets/wallet2.json")).expect("Can`t read kp wallet 2 file");
+    println!("Wallet 2 balance: {:?}", client.get_balance(&wal2.pubkey()));
+
+    println!("Airdrop to wal 1: {:?}", client.request_airdrop(&wal1.pubkey(), to_lamports(1)));
+    println!("Wallet 1 balance: {:?}", client.get_balance(&wal1.pubkey()));
+    
 }
